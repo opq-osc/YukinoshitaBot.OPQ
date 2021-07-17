86e6bcd00285545faa1d51d3301d41fc3f44064c
李卓强 <1137361788@qq.com>
2021-07-18 00:02:52 +0800

    添加项目文件。

---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace YukinoshitaBot
+{
+    using System;
+
+    /// <summary>
+    /// This is the main class of the application.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
